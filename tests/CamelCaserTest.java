8481e4fed7613b5b07a7c350efb832e088e273d5
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-22 01:18:20 -0600

    Wrote some sample test cases

--- tests/CamelCaserTest.java
+++ tests/CamelCaserTest.java
@@ -7,5 +7,9 @@ public class CamelCaserTest {
     @Test
     public void handoutTestCamelCase() {
         assertEquals("thisIsMyString", CamelCaser.camelCase("this is my string"));
+        assertEquals("iAmTheBest", CamelCaser.camelCase("I am the best"));
+        assertEquals(" HAHAHA", CamelCaser.camelCase("hahaha"));
+        
+
     }
 }
