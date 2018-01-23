06da8ea14cc5035749d60819ac74c756ded4cafa
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-23 15:36:56 -0600

    All done
    Signed-off-by: Aishik Ghosh <aishik.ghosh@outlook.com>

--- src/CamelCaser.java
+++ src/CamelCaser.java
@@ -1,21 +1,16 @@
 public class CamelCaser{
         public static String camelCase(String inputString){
 
-
         if(inputString==null){ //Getting the specifications out of the way
             throw new IllegalArgumentException(ErrorConstants.NULL_INPUT);
         }
-
         if(inputString.equals("")){ //Getting those edge cases out of the way
             return(inputString); //returns if empty string is sent in
         }
+        inputString = inputString.trim();
         String lowercase = inputString.toLowerCase(); //converting everything to lower case so that CAPS never bothers me
-        while(lowercase.substring(0,1).equals(" ")){ //if space if the first thing the user inputs, it breaks my code. Now it doesn't
-            lowercase=lowercase.substring(0+1); //ignore it and start from whenever its not a space
-        }
         char[]arr=lowercase.toCharArray(); //take everything to char array
         StringBuffer sb=new StringBuffer(); //easier to fix bufferStrings according to StackExchange
-
 
         for(int i=0;i<arr.length;i++){
             if(!(Character.isLetter(arr[i])||Character.isDigit(arr[i])||Character.isWhitespace(arr[i]))){
@@ -30,10 +25,10 @@ public class CamelCaser{
             }
             if(arr[i]!= ' '){
                 sb.append(arr[i]); //joins the word together if there is space
-
             }
         }
         String ha= sb.toString(); //converts it back to a string, because strings are cool
-        return ha; //returns the the string
+
+        return ha;
     }
 }
--- tests/CamelCaserTest.java
+++ tests/CamelCaserTest.java
@@ -76,6 +76,7 @@ The lower camel case version of the string is defined as follows.
     public void IrdinaTest() { // A friend challenged my code and wrote this test case and ofc it passed :)
         assertEquals("lolhi", CamelCaser.camelCase("lolhi"));
         assertEquals("whatSp8cesAreExisting", CamelCaser.camelCase("   What     sp8Ces  aRE            existing"));
+        assertEquals("whatSp8cesAre", CamelCaser.camelCase("   What     sp8Ces  aRE            ")); //space after
     }
 
     @Test
