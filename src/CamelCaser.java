46158f7f8c389f7b75a663ac0351e9f7629b1d79
Aishik Ghosh <aishik.ghosh@outlook.com>
2018-01-23 20:16:44 -0600

    All done v2
    Signed-off-by: Aishik Ghosh <aishik.ghosh@outlook.com>

--- src/CamelCaser.java
+++ src/CamelCaser.java
@@ -7,7 +7,7 @@ public class CamelCaser{
         if(inputString.equals("")){ //Getting those edge cases out of the way
             return(inputString); //returns if empty string is sent in
         }
-        inputString = inputString.trim();
+        inputString = inputString.trim(); //trimming so spaces in front and after do not bother me
         String lowercase = inputString.toLowerCase(); //converting everything to lower case so that CAPS never bothers me
         char[]arr=lowercase.toCharArray(); //take everything to char array
         StringBuffer sb=new StringBuffer(); //easier to fix bufferStrings according to StackExchange
@@ -28,7 +28,6 @@ public class CamelCaser{
             }
         }
         String ha= sb.toString(); //converts it back to a string, because strings are cool
-
         return ha;
     }
 }
